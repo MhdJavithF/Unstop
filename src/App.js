16d44accf3abe34eff53d6804c2fe0ecc6243f70
MhdJavithF <javithmd001@gmail.com>
2025-01-19 01:34:34 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Login from './Pages/Login';
 import Home from './Pages/Home';
 
 function App() {
-  const isAuthenticated = localStorage.getItem('isAuthenticated') == 'true';
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const authStatus = localStorage.getItem('isAuthenticated') == 'true';
+    setIsAuthenticated(authStatus);
+  }, []);
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/auth/login" />} />
-        
+        <Route path="/" element={<Navigate to={isAuthenticated ? "/home" : "/auth/login"} />} />
         <Route path="/auth/login" element={<Login />} />
         <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/auth/login" />} />
       </Routes>
@@ -17,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
